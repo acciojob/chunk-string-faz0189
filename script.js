df331b378d5727805c3e04e1b95ac86ba8f811c4
faz0189 <128984518+faz0189@users.noreply.github.com>
2025-03-07 18:52:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
-function stringChop(str, size) {
-  // your code here
+function chunkString(str, length) {
+    // Return an empty array if the input string is null
+    if (!str) return [];
+
+    let chunks = [];
+
+    // Loop through the string in increments of the given length
+    for (let i = 0; i < str.length; i += length) {
+        chunks.push(str.slice(i, i + length));
+    }
+
+    return chunks;
 }
 
-// Do not change the code below
-const str = prompt("Enter String.");
-const size = prompt("Enter Chunk Size.");
-alert(stringChop(str, size));
